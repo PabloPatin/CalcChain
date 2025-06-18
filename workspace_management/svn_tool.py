93f1540dc07944376728aecbae923dea29992f22
PabloPatin <pashka.lop@mail.ru>
2025-06-18 11:34:20 +0000

    Изменён метод load_file_from_config, теперь он возвращает объект info с информацией о загруженном файле
    git-svn-id: svn://localhost/trunk@28 5ecc171e-b03b-8e4e-9c5a-ba543eed454b

--- workspace_management/svn_tool.py
+++ workspace_management/svn_tool.py
@@ -2,7 +2,7 @@ import os.path
 
 from workspace_management.abstract import AbstractManager
 
-from svn import SvnClient
+from svn import SvnClient, data_structures
 
 
 class SvnTool(AbstractManager):
@@ -10,13 +10,14 @@ class SvnTool(AbstractManager):
         super().__init__(path_to_workspace)
         self.svn_client = None
 
-    def load_file_from_config(self, from_link: str, to_local_path: str = '',
+    def load_file_from_config(self, from_link: str, to_local_dir: str = '',
                               revision: int | None = None, local_file_name: str | None = None,
-                              **kwargs) -> int: # noqa ARG002 ARG003
+                              **kwargs) -> data_structures.Info: # noqa ARG002 ARG003
         """
         Метод загружает в рабочее пространство файл из репозитория
         :arg from_link: Ссылка на файл в репозитории
-        :param to_local_path: Путь относительно корня рабочей области
+        :param to_local_dir: Путь к директории относительно корня рабочей области,
+                             куда поместить файл
         :param revision: Ревизия репозитория (по умолчанию HEAD)
         :param local_file_name: Имя файла в рабочей области (если надо переименовать)
         :param kwargs: Прочие аргументы, которые могу быть в словаре с конфигурацией
@@ -25,8 +26,8 @@ class SvnTool(AbstractManager):
         repo_link, rel_path = self.split_link(from_link)
         if not local_file_name:
             local_file_name = rel_path
-        local_path = os.path.join(self.work_path, to_local_path, local_file_name)
+        local_path = os.path.join(self.work_path, to_local_dir, local_file_name)
         self.svn_client = SvnClient(repo_link)
         self.svn_client.export(rel_path, local_path, revision=revision)
-        return revision if revision else self.svn_client.info(rel_path).commit_revision
+        return self.svn_client.info(rel_path)
 
