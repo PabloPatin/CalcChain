2145806993fe27f632c86ad305b088884d46da68
PabloPatin <pashka.lop@mail.ru>
2025-06-18 12:54:14 +0000

    Поправил область видимости переменной класса
    git-svn-id: svn://localhost/trunk@30 5ecc171e-b03b-8e4e-9c5a-ba543eed454b

--- workspace_management/abstract.py
+++ workspace_management/abstract.py
@@ -2,7 +2,7 @@ from abc import ABCMeta
 
 
 class AbstractManager(metaclass=ABCMeta):
-    __exec_rel_path = 'exec'
+    _exec_rel_path = 'exec'
 
     def __init__(self, path_to_workspace: str):
         self.work_path = path_to_workspace
