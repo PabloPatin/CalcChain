be8256b31c3d3e2cca9f515e7727ff1c7c3fe395
PabloPatin <pashka.lop@mail.ru>
2025-06-18 11:33:13 +0000

    Добавлен класс LocalDeviceTool
    git-svn-id: svn://localhost/trunk@27 5ecc171e-b03b-8e4e-9c5a-ba543eed454b

---
+++ workspace_management/local_device_tool.py
@@ -0,0 +1,22 @@
+import os
+
+from workspace_management.abstract import AbstractManager
+import shutil
+
+
+class LocalDeviceTool(AbstractManager):
+    def load_file_from_config(self, from_path: str, to_local_dir: str = '',
+                              local_file_name: str | None = None,
+                              **kwargs) -> int:  # noqa ARG002 ARG003
+        """
+        Метод загружает в рабочее пространство файл из файловой системы устройства
+        :arg from_path: Путь к файлу на устройстве
+        :param to_local_dir: Путь к директории относительно корня рабочей области,
+                             куда поместить файл
+        :param local_file_name: Имя файла в рабочей области (если надо переименовать)
+        :param kwargs: Прочие аргументы, которые могу быть в словаре с конфигурацией
+        """
+        local_path = os.path.join(self.work_path, to_local_dir)
+        if local_file_name:
+            local_path = os.path.join(local_path, local_file_name)
+        shutil.copy2(from_path, local_path)
--- workspace_management/manager.py
+++ workspace_management/manager.py
@@ -3,6 +3,7 @@ import tomllib
 from typing import Any
 
 from .abstract import AbstractManager
+from .local_device_tool import LocalDeviceTool
 from .svn_tool import SvnTool
 
 
@@ -17,6 +18,7 @@ class IncorrectDirectoryError(Exception):
 class ConfigNotFoundError(Exception):
     pass
 
+
 class WorkspaceManager(AbstractManager):
     __config_name = 'config.toml'
 
@@ -24,15 +26,18 @@ class WorkspaceManager(AbstractManager):
         super().__init__(path_to_workspace)
         self.__check_workspace_init()
         self.configs = self.read_toml()
+        self.info = {}
         self.__exec_path = os.path.join(self.work_path, self.__exec_rel_path)
         self.svn_tool = SvnTool(self.work_path)
+        self.local_device_tool = LocalDeviceTool(self.work_path)
 
     def __check_workspace_init(self) -> None:
         if not os.path.exists(self.work_path):
             raise PathNotFoundError(f'Не найдена директория по пути {self.work_path}')
         workspace_content = os.listdir(self.work_path)
-      # if not all([path.lower().endswith('.toml') for path in workspace_content]):
-      #     raise IncorrectDirectoryError(f'Директория должна содержать только файлы формата TOML')
+        # if not all([path.lower().endswith('.toml') for path in workspace_content]):
+        #     raise IncorrectDirectoryError(
+        #             f'Директория должна содержать только файлы формата TOML')
         if workspace_content != [self.__config_name]:
             sep = '\n'
             raise IncorrectDirectoryError(f'Директория должна содержать только файл config.toml\n'
@@ -42,12 +47,9 @@ class WorkspaceManager(AbstractManager):
         with open(os.path.join(self.work_path, self.__config_name), 'rb') as file:
             return tomllib.load(file)
 
-    def load_all_exec(self) -> None:
-        for exec_name in self.configs['exec']:
-            self.load_exec(exec_name)
-
     @staticmethod
-    def try_get(dictionary: dict, key: str, *keys: str, error_message: str = '') -> Any: # noqa ANN401
+    def try_get_config(dictionary: dict, key: str, *keys: str,
+                       error_message: str = '') -> Any:  # noqa ANN401
         keys = (key, *keys)
         try:
             for key in keys:
@@ -57,22 +59,30 @@ class WorkspaceManager(AbstractManager):
         else:
             return value
 
+    def load_all_exec(self) -> None:
+        for exec_name in self.try_get_config(self.configs, 'exec'):
+            self.load_exec(exec_name)
+
     def load_exec(self, exec_name: str) -> None:
-        if not os.path.exists(self.__exec_path) or not os.path.isdir(self.__exec_path):
-            os.mkdir(self.__exec_path)
-        exec_config = self.try_get(self.configs, 'exec', exec_name,
+        """
+        Загружает в рабочее пространство исполняемый файл, указанный в конфигурации
+        :param exec_name: Имя программы, указанное в конфигурационном файле
+        """
+        os.makedirs(self.__exec_path, exist_ok=True)
+        exec_config = self.try_get_config(self.configs, 'exec', exec_name,
                                    f'Не найдена конфигурация для {exec_name}')
         svn_config = exec_config.get('svn')
         local_config = exec_config.get('local')
+        local_path = os.path.join(self.__exec_path, exec_name)
+
         if isinstance(svn_config, dict):
-            return self.svn_tool.load_file_from_config(**svn_config, to_local_path=
-                                                       os.path.join(self.__exec_path, exec_name))
+            self.load_exec_from_svn(svn_config, local_path)
         elif isinstance(local_config, dict):
-            return self.load_exec_local(local_config)
+            self.local_device_tool.load_file_from_config(**local_config, to_local_dir=local_path)
         else:
             raise ConfigNotFoundError(f'Конфигурация для {exec_name} некорректна')
 
-
-    def load_exec_local(self, local_config: dict) -> None:
-        # TODO: Сделать, когда напишу тесты
-        pass
+    def load_exec_from_svn(self, svn_config: dict, local_path: str) -> None:
+        file_info = self.svn_tool.load_file_from_config(**svn_config, to_local_dir=local_path)
+        revision = file_info.commit_revision
+        svn_config['revision'] = revision
