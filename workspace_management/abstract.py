08de39b7d9763b5b2fad8f1fc0eb05c18a90b6a0
PabloPatin <pashka.lop@mail.ru>
2025-06-18 10:25:34 +0000

    Написал докстринги, перенёс метод split_link в абстрактный класс, сделал так, чтобы load_file_from_config возвращал номер ревизии
    git-svn-id: svn://localhost/trunk@24 5ecc171e-b03b-8e4e-9c5a-ba543eed454b

--- workspace_management/abstract.py
+++ workspace_management/abstract.py
@@ -6,3 +6,12 @@ class AbstractManager(metaclass=ABCMeta):
 
     def __init__(self, path_to_workspace: str):
         self.work_path = path_to_workspace
+
+    def split_link(self, link: str) -> tuple[str, str]:
+        """
+        Делит ссылку на путь до объекта ие его название
+        :param link: Ссылка или путь
+        :return: Кортеж вида ([путь до объекта], [имя объекта])
+        """
+        last_sep = max(link.rfind('/'), link.rfind('\\'))
+        return link[:last_sep], link[last_sep + 1:]
--- workspace_management/svn_tool.py
+++ workspace_management/svn_tool.py
@@ -10,16 +10,23 @@ class SvnTool(AbstractManager):
         super().__init__(path_to_workspace)
         self.svn_client = None
 
-    def split_link(self, link: str) -> tuple[str, str]:
-        last_sep = max(link.rfind('/'), link.rfind('\\'))
-        return link[:last_sep], link[last_sep + 1:]
-
     def load_file_from_config(self, from_link: str, to_local_path: str = '',
                               revision: int | None = None, local_file_name: str | None = None,
-                              **kwargs) -> None: # noqa ARG002 ARG003
+                              **kwargs) -> int: # noqa ARG002 ARG003
+        """
+        Метод загружает в рабочее пространство файл из репозитория
+        :arg from_link: Ссылка на файл в репозитории
+        :param to_local_path: Путь относительно корня рабочей области
+        :param revision: Ревизия репозитория (по умолчанию HEAD)
+        :param local_file_name: Имя файла в рабочей области (если надо переименовать)
+        :param kwargs: Прочие аргументы, которые могу быть в словаре с конфигурацией
+        :return: Возвращает номер ревизии файла
+        """
         repo_link, rel_path = self.split_link(from_link)
         if not local_file_name:
             local_file_name = rel_path
         local_path = os.path.join(self.work_path, to_local_path, local_file_name)
         self.svn_client = SvnClient(repo_link)
         self.svn_client.export(rel_path, local_path, revision=revision)
+        return revision if revision else self.svn_client.info(rel_path).commit_revision
+
