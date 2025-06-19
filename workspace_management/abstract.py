14a05cba7dda6bc2a1e12177f254227b166f6bda
PabloPatin <pashka.lop@mail.ru>
2025-06-19 07:50:13 +0000

    Убрал путь к исполняемым файлам из абстрактного класса
    git-svn-id: svn://localhost/trunk@35 5ecc171e-b03b-8e4e-9c5a-ba543eed454b

--- workspace_management/abstract.py
+++ workspace_management/abstract.py
@@ -2,11 +2,6 @@ from abc import ABCMeta
 
 
 class AbstractManager(metaclass=ABCMeta):
-    _exec_rel_path = 'exec'
-
-    def __init__(self, path_to_workspace: str):
-        self.work_path = path_to_workspace
-
     def split_link(self, link: str) -> tuple[str, str]:
         """
         Делит ссылку на путь до объекта ие его название
