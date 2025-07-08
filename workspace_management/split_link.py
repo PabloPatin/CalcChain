36eebf0f8cbc49f9b742f07f97dc04254e80ecab
PabloPatin <pashka.lop@mail.ru>
2025-07-08 07:10:02 +0000

    Удалён лишний модуль
    git-svn-id: svn://localhost/trunk@79 5ecc171e-b03b-8e4e-9c5a-ba543eed454b

--- workspace_management/split_link.py
+++
@@ -1,8 +0,0 @@
-def split_link(link: str) -> tuple[str, str]:
-    """
-    Делит ссылку на путь до объекта ие его название
-    :param link: Ссылка или путь
-    :return: Кортеж вида ([путь до объекта], [имя объекта])
-    """
-    last_sep = max(link.rfind('/'), link.rfind('\\'))
-    return link[:last_sep], link[last_sep + 1:]
