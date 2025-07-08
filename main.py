efb78d32485b030f766003adcae000430a43be81
PabloPatin <pashka.lop@mail.ru>
2025-07-08 13:55:29 +0000

    Обновлён парсинг аргументов: добавлена помощь для каждого метода
    git-svn-id: svn://localhost/trunk@92 5ecc171e-b03b-8e4e-9c5a-ba543eed454b

--- main.py
+++ main.py
@@ -4,17 +4,46 @@ from argparse import Namespace
 from workspace_management import WorkspaceManager
 
 
-def main() -> None:
-    args = parse_args()
-    match args.command:
-        case 'init-ws':
-            manager = WorkspaceManager(args.path, check_init=True)
-            init_workspace(manager)
-        case 'check-ws':
-            manager = WorkspaceManager(args.path)
-            check_files(manager)
-        case _:
-            print(parser.print_help())
+def parse_args(parser: argparse.ArgumentParser) -> Namespace:
+    parent_parser = argparse.ArgumentParser(add_help=False)
+    parent_parser.add_argument(
+            '-p', '--path',
+            type=str,
+            metavar='WS_PATH',
+            help='Путь к рабочей области',
+            required=True,
+            )
+
+    subparsers = parser.add_subparsers(
+            dest='command',
+            title='Команды программы',
+            required=True,
+            metavar='{init-ws, check-ws}'
+            )
+
+    subparsers.add_parser(
+            'init-ws', aliases=['initialise-workspace'],
+            help='Создать рабочую область',
+            parents=[parent_parser]
+            )
+
+    hash_check_parser = subparsers.add_parser(
+            'check-ws', aliases=['check-workspace-file-hashes'],
+            help='Проверить наличие изменённых файлов',
+            description='Сверяет хэши всех файлов в рабочей области '
+                        'и находит файлы, которые были изменены.',
+            parents=[parent_parser],
+            )
+    hash_check_parser.add_argument(
+            '--ignore',
+            nargs='*',
+            help='Пути в рабочей области, которые не надо проверять',
+            type=str,
+            metavar='PATH'
+            )
+
+    args = parser.parse_args()
+    return args
 
 
 def init_workspace(manager: WorkspaceManager) -> None:
@@ -26,29 +55,29 @@ def init_workspace(manager: WorkspaceManager) -> None:
     manager.save_ws_info()
 
 
-def check_files(manager: WorkspaceManager) -> None:
+def check_files(manager: WorkspaceManager, ignore: list[str] | None = None) -> None:
     info = manager.load_ws_info()
-    changed_files = manager.check_hashes(info.hash_sums)
+    changed_files = manager.check_hashes(info.hash_sums, ignore=ignore)
     if changed_files:
         print('Файлы изменены:')
     for file in changed_files:
         print(file)
 
 
-def parse_args() -> Namespace:
-    subparsers = parser.add_subparsers(dest='command', help='Доступные команды')
-
-    initial_parser = subparsers.add_parser('init-ws', help='Инициализатор рабочей области')
-    initial_parser.add_argument('-p', '--path', type=str)
-
-    hash_check_parser = subparsers.add_parser('check-ws', help='Проверка изменения файлов')
-    hash_check_parser.add_argument('-p', '--path', type=str)
-    hash_check_parser.add_argument('--skip-files', nargs='+', type=str)
-
-    args = parser.parse_args()
-    return args
-
-
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    main()
+    parser = argparse.ArgumentParser(
+            prog='calc_manager',
+            description='Среда для запуска расчётных скриптов',
+            )
+
+    args = parse_args(parser)
+
+    match args.command:
+        case 'init-ws':
+            manager = WorkspaceManager(args.path, check_init=True)
+            init_workspace(manager)
+        case 'check-ws':
+            manager = WorkspaceManager(args.path)
+            check_files(manager, args.ignore)
+        case _:
+            print(parser.print_help())
