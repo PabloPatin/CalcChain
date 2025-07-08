e281d0796503be6563ea3ee187d1187ebacff2df
PabloPatin <pashka.lop@mail.ru>
2025-07-08 11:29:21 +0000

    Обработка ошибок
    git-svn-id: svn://localhost/trunk@89 5ecc171e-b03b-8e4e-9c5a-ba543eed454b

--- svn/commander.py
+++ svn/commander.py
@@ -2,6 +2,7 @@ import logging
 import os
 import subprocess
 from collections.abc import Sequence
+from pathlib import Path
 
 from .config import LANG
 from .exception import SvnError
@@ -12,7 +13,7 @@ _LOGGER = logging.getLogger(__name__)
 class Commander:
     def external_command(
             self, cmd: str | Sequence[str], split_lines: bool = False, return_binary: bool = False,
-            environment: dict | None = None, wd: str | None = None,
+            environment: dict | None = None, wd: str | Path | None = None,
             join_stderr: bool = False, encoding: str | None = None) -> str | list[str] | bytes:
         """Выполнение команд в консоли и обработка ошибок
 
@@ -25,13 +26,16 @@ class Commander:
         :param encoding:"""
         _LOGGER.debug(f'RUN: {cmd}')
         env = self.set_env(environment)
+        wd = Path(wd) if wd else Path()
+        if not wd.is_dir():
+            raise FileNotFoundError('Рабочая директория не найдена')
 
         if return_binary:
             encoding = None
 
         p = subprocess.Popen(
                 cmd,
-                cwd=wd,
+                cwd=wd.resolve(),
                 env=env,
                 stdout=subprocess.PIPE,
                 universal_newlines=not return_binary,
